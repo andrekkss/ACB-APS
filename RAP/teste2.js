25934cea24f73b6dc3815bfe645e364f1a9a7f11
André Kitano <andre.kitano.silva@gmail.com>
2019-04-18 23:11:45 -0300

    console log

--- RAP/teste2.js
+++ RAP/teste2.js
@@ -2,6 +2,7 @@ var request = require('request');
 var apikey = "fde01de895b2bcfcf4caa5183e034456";
 request('http://api.openweathermap.org/data/2.5/weather?q=Madrid&APPID=' +apikey, function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred
+  console.log(`Bigode: ${error}`); // <------------ esse aqui bigode
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the Google homepage.
 });
