c66dd93efbddb3d7395d42db793f12d834c92123
André Kitano <andre.kitano.silva@gmail.com>
2019-04-19 16:33:08 -0300

    polindo codigo

--- server/src/routes/socket.js
+++ server/src/routes/socket.js
@@ -2,22 +2,22 @@
 module.exports = {
     start: function(io){
         var clients = {}; 
-        io.on("connection", function (socket) {  
+        io.on("connection", function(socket) {  
             socket.on("join", function(name){
-            console.log("Joined: " + name);
+            console.log(`Entrou: ${name}`);
             clients[socket.id] = name;
-            socket.emit("update", "You have connected to the server.");
-            socket.broadcast.emit("update", name + " has joined the server.")
+            socket.emit("update", "Você foi desconectado do servidor.");
+            socket.broadcast.emit("update", name + " Conectado.")
         });
         socket.on("send", function(msg){
-            console.log("Message: " + msg);
+            console.log("Messagem: " + msg);
             socket.broadcast.emit("chat", clients[socket.id], msg);
         });
         socket.on("disconnect", function(){
-            console.log("Disconnect");
-            io.emit("update", clients[socket.id] + " has left the server.");
+            console.log("Disconnectado ");
+            io.emit("update", clients[socket.id] + " Saiu.");
             delete clients[client.id];
           });
         });
-    },
+    }
 };
