afb8e43cc11ccfb0fab0d2934d838cbb798e9de1
GMustache <37159515+GMustache@users.noreply.github.com>
2019-04-18 23:20:03 -0300

    Update teste2.js

--- RAP/teste2.js
+++ RAP/teste2.js
@@ -1,6 +1,6 @@
 var request = require('request');
 var apikey = "fde01de895b2bcfcf4caa5183e034456";
-request('http://api.openweathermap.org/data/2.5/weather?q=Madrid&APPID=' +apikey, function (error, response, body) {
+request(`http://api.openweathermap.org/data/2.5/weather?q=Madrid&APPID=${apikey}` , function (error, response, body) {
   console.log('error:', error); // Mostra um erro caso ocorra.
   console.log(`Bigode: ${error}`); // <------------ esse aqui bigode
   console.log('statusCode:', response && response.statusCode); //Mostra o status de resposta.
